ced9cd5d0d75666fa46502040a8b158ccc0db0a2
younha-kim <rladudtmd727@gmail.com>
2022-05-15 15:55:45 +0900

    new file

---
+++ 패캠/mainjs.js
@@ -0,0 +1,19 @@
+console.log('Herpoy') ;
+
+let myName = "HEROPY";
+let email = 'thesecon@gmail.com';
+let hello = `hello ${myName}?`
+    /* 보관법을 사용하면 중간에 다른데이터를 끼워 넣을 수 있다.
+     */
+console.log(email);
+console.log(hello);
+
+
+let undef;
+let obj = { abc: 123} ;
+
+console.log(undef) ; // undefined.
+console.log(obj.abc) ;
+console.log(obj.xyz);  
+
+
